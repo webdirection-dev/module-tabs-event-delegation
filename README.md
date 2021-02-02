f72bb33bb819aa789409671d65c7b7e7b2c2d300
MacAir <git@webdirection.org>
2021-02-02 16:41:21 +0300

    BEGINING

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # module-tabs-event-delegation
- 
+Модуль содержит скрипт для создание табов.
+
+> Применяются принципы ___Делигирования событий___.
+
+<br />
+
+## Дерево файлов и каталогов
+Необходимо сформировать файловое дерево в соответствие с настройками ___Webpack___ и ___GulpJS___. Данные настройки находятся в одноименных файлах: ___webpack.config.js___ и ___gulpfile.js___, если таковые имеются в репозитории.
+
+<br />
+
+![files](./dist/img/files.png)
+
+> Важно! В подключаемых файлах нужно прописать правильные пути. Большинство ошибок допускаются в путях.
+
+<br />
+
+## Подключение модуля
+В данном репозитории точкой вхождения ___Webpack___ является _./js/script.js_.
+<br /> Импортируем в него модуль со скриптом слайдера:
+```javascript
+// Точка вхождения Webpack: "./js/script.js'
+"use strict";
+
+import tabs from './modules/tabs';
+window.addEventListener('DOMContentLoaded', () => {
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+});
+```
+<br />
+
+## Деструктуризация объекта с аргументами
+Функция slider() принимает объект следующих аргументов {}:
+
+| Аргумент | Селектор | Значение | 
+| :---- | :---- | :--- |
+| tabSelector | .tabheader__item | Кнопки для смены табов |
+| tabsContentSelector | .tabcontent | Часть конкретного таба с описанием |
+| tabsParentSelector | .tabheader__items | Пэрент, делегирующий события на чилдрен |
+| activeClass | .tabheader__item_active | Класс активности |
+
---
+++ dist/js/bundle.js
@@ -0,0 +1,141 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/tabs.js":
+/*!********************************!*\
+  !*** ./src/js/modules/tabs.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+// ТАБЫ
+
+function tabs(tabSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+    const tabs = document.querySelectorAll(tabSelector),
+        tabsContent = document.querySelectorAll(tabsContentSelector),
+        tabsParent = document.querySelector(tabsParentSelector);
+
+    // Скрываем табы
+    function hideTabContent() {
+        // Скрываем все блоки с контентом
+        tabsContent.forEach((item) => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+        // Убираем класс Active с выделенной кнопки
+        tabs.forEach((item) => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    // Показываем табы
+    function showTabContent(i = 0) {  // i = 0 дефолтное значение 0, где первый индекс псевдомассивов tabsContent и tabs;
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    // Дилегируем onclick на все табы tabs
+    tabsParent.addEventListener('click', (event) => {   // ОБЯЗАТЕЛЬНО передаем объект события event
+        const target = event.target;  // переопределяем even.target в переменную
+        if (target && target.classList.contains(tabSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (tabs);
+
+/***/ }),
+
+/***/ "./src/js/script.js":
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/tabs */ "./src/js/modules/tabs.js");
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_0__.default)('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/js/script.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
